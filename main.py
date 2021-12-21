d8097e9605630068026e29b2d7a82cb1f2cfcd79
Michelangelo <mhlmrt@gmail.com>
2021-12-21 16:45:31 +0100

    include speech_detection in main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # This is a sample Python script.
+import os
+
 from pydub import AudioSegment
 import glob
 import speech_recognition as sr
+import speech_detection as sd
 
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -11,6 +14,8 @@ def transcriptWav(filename):
     r = sr.Recognizer()
     with sr.AudioFile(filename) as source:
         try:
+            #calibrates the recognizer to the noise level of the audio
+            r.adjust_for_ambient_noise(source, 1)
             # listen for the data (load audio to memory)
             audio_data = r.record(source)
             # recognize (convert from speech to text)
@@ -19,24 +24,22 @@ def transcriptWav(filename):
         except:
             print("Error !")
 
-
-def readRTTM(filename):
+def readRTTM(filename, fileAudio):
     i = -1
     with open(filename) as file:
         for line in file:
             i = i + 1
             line = line.rstrip().split(' ')
-            print(line)
+            #print(line)
             t1 = float(line[3]) * 1000              # Works in milliseconds
             t2 = t1 + float(line[4]) * 1000
             if t2 - t1 < 500:
                 continue
-            print(str(t1) + " to " + str(t2))
+            #print(str(t1) + " to " + str(t2))
             newNameAudio = "Resources/Audio/Split/track" + str(i) + ".wav"
-            newAudio = AudioSegment.from_wav("Resources/Audio/track.wav")
+            newAudio = AudioSegment.from_wav(fileAudio)
             newAudio = newAudio[t1:t2]
             newAudio.export(newNameAudio, format="wav")  # Exports to a wav file in the current path.
-
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -45,20 +48,21 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    filename = 'Resources/speaker_diarization.rttm'
-    readRTTM(filename)
+    print_hi('PyCharm Start')
+    listAudio = glob.glob("Resources/Audio/track*.wav")
 
-    # All files ending with .txt
-    list = glob.glob("Resources/Audio/Split/track*.wav")
+    for list in listAudio:
+        #transcriptWav(list)
+        sd.diarization(list)
+        readRTTM(list.replace('.wav', '.rttm'), list)
+        listSplit = glob.glob("Resources/Audio/Split/track*.wav")
+        for line in listSplit:
+            transcriptWav(line)
 
-    print(list)
+        for line in listSplit:
+            os.remove(line)
+            os.remove(list.replace('.wav', '.rttm'))
 
-    #transcriptWav("Resource/Audio/track.wav")
-    for line in list:
-        print(line)
-        transcriptWav(line)
-    #transcriptWav("Resources/Audio/Split/track10.wav")
 
 
 
--- speech_detection.py
+++ speech_detection.py
@@ -1,16 +1,18 @@
 # load model and wrap it in a pipeline
 import torch
 
-model = ['sad_dihard','sad_ami','sad_etape'] #the latter may be unavailable
+def diarization(filename):
+    model = ['sad_dihard','sad_ami','sad_etape'] #the latter may be unavailable
 
-pipeline = torch.hub.load('pyannote/pyannote-audio', model[1], pipeline=True)
+    pipeline = torch.hub.load('pyannote/pyannote-audio', model[1], pipeline=True)
 
-# apply speech activity detection pipeline on your audio file
-speech_activity_detection = pipeline({'audio': 'track.wav'})
+    # apply speech activity detection pipeline on your audio file
+    speech_activity_detection = pipeline({'audio': filename})
 
-# dump result to disk using RTTM format
-with open('Resources/speech_detection.rttm', 'w') as f:
-    speech_activity_detection.write_rttm(f)
+    # dump result to disk using RTTM format
+    newFilename = filename.replace('.wav', '.rttm')
+    with open(newFilename, 'w') as f:
+        speech_activity_detection.write_rttm(f)
 
-for speech_region in speech_activity_detection.get_timeline():
-    print(f'There is speech between t={speech_region.start:.1f}s and t={speech_region.end:.1f}s.')
+    for speech_region in speech_activity_detection.get_timeline():
+        print(f'There is speech between t={speech_region.start:.1f}s and t={speech_region.end:.1f}s.')
